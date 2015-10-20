4502980af18adb5b6420ab9eca83efe93f6a8a6c
Ondřej Piálek <ondrej@pialek.eu>
2015-10-20 11:46:33 +0200

    Facebook implemented and tested. The date format might be wrong, needs integration testing with the plugin.

--- src/Endzone.UmbracoSocialPluginAdapters/API/FacebookController.cs
+++ src/Endzone.UmbracoSocialPluginAdapters/API/FacebookController.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Web.Http;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 
 namespace Endzone.UmbracoSocialPluginAdapters.API
 {
+    /// <summary>
+    /// Standard URL to access this controller is /umbraco/UmbracoSocialPluginAdapters/Facebook/
+    /// </summary>
     [PluginController(Constants.AreaName)]
     public class FacebookController : UmbracoApiController
     {
+        /// <summary>
+        /// Standard URL to access this action is /umbraco/UmbracoSocialPluginAdapters/Facebook/Get.
+        /// 
+        /// The serialization logic only works with JSON requests. If you use a browser to test this action,
+        /// make sure you are sending application/json in your Accept header.
+        /// </summary>
+        /// <remarks>The id paramater can be a string too, the page name or user alias (untested).</remarks>
         [HttpGet]
-        public object Get(long id, string feed = "feed", int limit = 20)
+        public object Get(string id, string feed = "feed", int limit = 20)
         {
             var appId = ConfigurationManager.AppSettings[Constants.FacebookAppIdKey];
             var appSecret = ConfigurationManager.AppSettings[Constants.FacebookAppSecretKey];
-
-            var appAccessToken = $"{appId}|{appSecret}";
 
             var pageId = id;
 
@@ -31,8 +37,10 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
 
             var authUrl = $"https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id={appId}&client_secret={appSecret}";
 
-            var graphUrl = $"https://graph.facebook.com/v2.3/{pageId}/{feed}?key=value&access_token={appAccessToken}&fields={fields}&limit={limit}";
-            var pageUrl = $"https://graph.facebook.com/v2.3/{pageId}?key=value&access_token={appAccessToken}&fields=id,link,name";
+            var accessToken = new WebClient().DownloadString(authUrl);
+            accessToken = accessToken.Substring(accessToken.IndexOf('=') + 1);
+
+            var pageUrl = $"https://graph.facebook.com/v2.3/{pageId}?key=value&access_token={accessToken}&fields=id,link,name";
 
             var pageObject = new WebClient().DownloadString(pageUrl);
 
@@ -41,11 +49,10 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
             var pageLink = pageDetails.link ?? string.Empty;
             var pageName = pageDetails.name ?? string.Empty;
 
+            var graphUrl = $"https://graph.facebook.com/v2.3/{pageId}/{feed}?key=value&access_token={accessToken}&fields={fields}&limit={limit}";
             var graphObject = new WebClient().DownloadString(graphUrl);
             dynamic parsedJson = JObject.Parse(graphObject);
             var pagefeed = parsedJson.data;
-
-            var count = 0;
 
             var response = new
             {
@@ -60,11 +67,11 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
             foreach (var data in pagefeed)
             {
                 string message;
-                if (!string.IsNullOrEmpty(data.message))
+                if (!string.IsNullOrEmpty((string)data.message))
                 {
                     message = ((string) data.message).Replace("\n", "<br />");
                 }
-                else if (!string.IsNullOrEmpty(data.story))
+                else if (!string.IsNullOrEmpty((string)data.story))
                 {
                     message = data.story;
                 }
@@ -73,16 +80,16 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
                     message = string.Empty;
                 }
 
-                if (!string.IsNullOrEmpty(data.description))
+                if (!string.IsNullOrEmpty((string)data.description))
                 {
                     message += " " + data.description;
                 }
 
-                string link = !string.IsNullOrEmpty(data.link) ? data.link : string.Empty;
-                string image = !string.IsNullOrEmpty(data.picture) ? data.picture : string.Empty;
-                string type = !string.IsNullOrEmpty(data.type) ? data.type : string.Empty;
+                string link = data.link ?? string.Empty;
+                string image = data.picture ?? string.Empty;
+                string type = data.type ?? string.Empty;
 
-                if (type == "status" && !string.IsNullOrEmpty(data.story))
+                if (type == "status" && !string.IsNullOrEmpty((string)data.story))
                 {
                     continue;
                 }
@@ -98,7 +105,7 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
 
                 if (data.object_id != null)
                 {
-                    if (!image.Contains("safe_image.php") && Regex.IsMatch(data.object_id, @"^\d+$"))
+                    if (!image.Contains("safe_image.php") && Regex.IsMatch((string)data.object_id, @"^\d+$"))
                     {
                         image = $"https://graph.facebook.com/{data.object_id}/picture?type=normal";
                     }
@@ -110,11 +117,11 @@ namespace Endzone.UmbracoSocialPluginAdapters.API
                     link,
                     content = message,
                     thumb = image,
-                    publishedDate = DateTime.Parse(data.created_time) //D, d M Y H:i:s O
+                    publishedDate = data.created_time //might need to change, original PHP date format: D, d M Y H:i:s O
                 });
             }
 
-            return pageObject;
+            return response;
         }
     }
 }
